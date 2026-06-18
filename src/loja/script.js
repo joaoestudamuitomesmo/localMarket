d2732d6774e2db5b58ea8df4601b0340b6c7e859
João Victor Marques da Silva <jmarquessva61@gmail.com>
2026-06-18 11:10:43 -0300

    Add files via upload

---
+++ src/loja/script.js
@@ -0,0 +1,7 @@
+let produtos
+
+window.onload = function() {
+    var storedUser = localStorage.getItem("usuario")
+    var user = JSON.Storage.getItem("usuario")
+    var storedUser = localStorage.getItem("usuario")
+}
